770ee3c8cee2eb8a718ea57dfc0369f1fd556b2c
IOTSE1 <paternill@gmail.com>
2017-06-04 13:06:40 +1000

    IotWeb pro added

---
+++ IoTWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(IoTWeb.Startup))]
+namespace IoTWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
